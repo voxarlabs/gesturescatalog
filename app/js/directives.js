4d1523bda424489bb5cd503e0ca83ba7e57ef1c1
Tomer Simis <tdl.simis@gmail.com>
2015-08-03 22:46:51 -0300

    Videos now showing thumbnails before loading YT player

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,3 +9,28 @@ gesturesApp.directive("filterDrawer", function() {
         }
     }
 });
+
+gesturesApp.directive("youtubeThumb", function($compile){
+	return {
+		restrict: "E",
+		scope: {
+			videoId: "=",
+			playerWidth: "=",
+			playerHeight: "="
+		},
+		template: '<div style="position:relative; overflow: hidden;  width: {{ playerWidth }}px; height: {{ playerHeight }}px">' +
+						'<img style="transform: scale(1.7); width: 100%; height: 100%;" src="http://i.ytimg.com/vi/{{ videoId }}/hqdefault.jpg" class="thumb">' +
+						'<a href="#" style="display:block; width: 50px; opacity: 0.7; height: 50px; background: url(img/play.png) no-repeat; background-size: cover; position: absolute; top: 50%; margin-top: -25px; left: 50%; margin-left: -25px"></a>' +
+					'</div>',
+		link: function(scope, elem, attrs){
+			$(elem).click(function(e){
+  				var html = '<iframe id="ytplayer" type="text/html"' +
+  							'width="' + scope.playerWidth +'" ' +
+  				 			'height="' + scope.playerHeight +'" ' +
+  				 			'src="http://www.youtube.com/embed/' + scope.videoId + '?autoplay=1" ' +
+  				 			'frameborder="0"/>';
+	            elem.replaceWith(html);
+			});
+		}
+	}
+})
